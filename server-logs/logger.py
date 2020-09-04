6a1f71fcf9ec9a274da5b656cd652da8081e1818
Nathaniel Cherian <ngcherian2014@gmail.com>
2020-09-04 10:01:19 -0700

    access log

--- server-logs/logger.py
+++ server-logs/logger.py
@@ -7,7 +7,7 @@ This code will read the access log from the Nighthawk Coders Website
 
 import re # importing the python regex module (built into python)
 
-with open("access.log") as access_log:
+with open("access.txt") as access_log:
     log_data = access_log.read()
 
 print("head", log_data[:200], "\n") #Print the first 200 characters of the access log, also known as a head
